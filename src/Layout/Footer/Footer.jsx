d8eb63f49fe985aa9f4d6bc59d6bcabb74bd1339
YourGitHubUsername <bezrukovp86@gmail.com>
2025-04-02 13:33:50 +0200

    Исправлены проблемы с футером и мобильной версткой: добавлено имя разработчика, улучшено отображение изображений, оптимизирована адаптивность

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -6,19 +6,8 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container">
         <div className="footer__developer">
-          <p className="footer__developer-text">
-            Crafted with passion and precision
-            <span>
-              Designed & Developed by{" "}
-              <a
-                href="https://github.com/Iusif797"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Iusif Mamedov
-              </a>
-            </span>
-          </p>
+          <span className="footer__developer-text">Разработано</span>
+          <span className="footer__developer-name">Eveline</span>
         </div>
       </div>
     </footer>
