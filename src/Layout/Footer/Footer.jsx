53b96f08b59a8740f6fd25f0c81242aa8b3b113c
YourGitHubUsername <bezrukovp86@gmail.com>
2025-04-02 13:37:33 +0200

    Исправлено отображение изображений и добавлено имя разработчика: обновлены стили для корректного отображения на всех устройствах, добавлено имя Iusif Mamedov в футер

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__container">
+      <div className="container">
         <div className="footer__developer">
           <span className="footer__developer-text">Разработано</span>
-          <span className="footer__developer-name">Eveline</span>
+          <span className="footer__developer-name">Iusif Mamedov</span>
         </div>
       </div>
     </footer>
